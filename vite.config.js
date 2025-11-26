95bd49f8b01c4214d786ecf671b23690f88c3c92
Jeffy John <jjeffy@gmail.com>
2025-11-26 14:56:43 +0530

    Resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
- base: '/shreyavarma.com/',  
-=======
-  base: '/shreyavarma.com/',     // <--- add this line
->>>>>>> 0903e3e8c40df8e5985b7ab3f94be40fcece8fb6
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  build: {
-    minify: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          animations: ['framer-motion'],
-        }
-      }
-    }
-  }
-})
-
+  base: '/shreyavarma.com/',
   plugins: [react()],
   server: {
     port: 3000
