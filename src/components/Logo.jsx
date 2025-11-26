d423c62f3625bd5d87de965229309a194f4e2d0c
Jeffy John <jjeffy@gmail.com>
2025-11-26 18:31:03 +0530

    Deploy Vite production build

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Logo({ className = "w-12 h-12" }) {
   return (
-    <img src="/shreyavarma.com/logo.svg" alt="Site Logo" className={className} />
+    <img src="/logo.svg" alt="Site Logo" className={className} />
   );
 }
 
