0903e3e8c40df8e5985b7ab3f94be40fcece8fb6
jeffyjjohn <jjeffy@gmail.com>
2025-11-26 14:45:19 +0530

    Add base path configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shreyavarma.com/',     // <--- add this line
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    minify: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          animations: ['framer-motion'],
+        }
+      }
+    }
+  }
+})
+
   plugins: [react()],
   server: {
     port: 3000
